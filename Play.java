c7a4b642cd091021211ad14a521dae0e57bb291a
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-09-14 15:24:55 +0200

    Code cleaning plus screen captures for the README

--- Play.java
+++ Play.java
@@ -3,58 +3,69 @@ import java.util.Random;
 
 class Play {
 
-    // Card drawing method
     public static String drawCard (String visibility) {
+
         String cards[] = {"Ace","2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
         Random r = new Random();
         int randomIndex = r.nextInt(13);
         String drawnCard = cards[randomIndex];
         if (visibility == "faceUp") {
+
             if (drawnCard == "Ace") {
+
                 System.out.println("An Ace is drawn.");                               
             } else {
+
                 System.out.println("A " + drawnCard + " is drawn.");
             }
         } else if (visibility == "faceDown") {
+
                 System.out.println("A face-down card is drawn.");
             }        
         return drawnCard;  
     }
 
-    // Point assignment method
     public static int assignPoints (int score, String card) {
+
         int earnedPoints = 0;
         if (card == "Ace" && score < 11) {
+
             System.out.println("Which value do you want to asign: 1 or 11?");
             int aceAnswer = input.nextInt();
             if (aceAnswer == 1) {
+
                 earnedPoints = 1;
                 System.out.println("1 point is earned.");
             } else if (aceAnswer == 11) {
+
                 earnedPoints = 11;
                 System.out.println("11 points are earned.");
-            } else { 
+            } else {
+
                 System.out.println("Your answer isn't valid! 11 points are earned.");
                 earnedPoints = 11;
             }
+
         } else if (card == "10" || card == "Jack" || card == "Queen" || card == "King") {
+
                 earnedPoints = 10;
                 System.out.println("10 points are earned.");
         } else if (card == "Ace" && score >= 11) {
+
                 earnedPoints = 1;
                 System.out.println("1 point is earned.");
         } else {
+
             earnedPoints = Integer.parseInt(card);
             System.out.println(card + " points are earned.");
         }
         return earnedPoints;
     }
 
-    // Scanning method
     public static Scanner input = new Scanner(System.in);
 
-    // Card distribution method for the Player
     public static int playerCardsDistribution() {
+
         int playerScore = 0;
         System.out.println("Player's turn. Two cards are drawn.");
         String playerCard1 = drawCard("faceUp");
@@ -65,8 +76,8 @@ class Play {
         return playerScore;
     }
 
-    // Card distribution method for the Dealer
     public static String[] dealerCardsDistribution() {
+
         int dealerScore = 0;
         System.out.println("Dealer's turn. Two cards are drawn: one face-up and one face-down.");
         String dealerCardFaceUp = drawCard("faceUp");
@@ -80,16 +91,16 @@ class Play {
         return tab;
     }
 
-    // Question to the Player whether he wants another card or not
     public static char askContinue() {
+
         System.out.println("Would you like another card? y or n");
         char continuePlayer = input.next().charAt(0);
         System.out.println();
         return continuePlayer;
     }
 
-    // Next card drawing method
     public static int drawNewCard(int score) {
+
         int playerScore = score;
         String newCard = drawCard("faceUp");
         int earnedPoints = assignPoints(playerScore, newCard);
@@ -98,8 +109,8 @@ class Play {
         return playerScore;
     }
 
-    // Procedure for drawing new cards
     public static int newCardProcess(int score, char answer) {
+
         int currentScore = score;   
         while ( currentScore <= 21 && answer == 'y' ) {
             currentScore = drawNewCard(currentScore);
@@ -117,24 +128,27 @@ class Play {
         return currentScore;
     }
 
-    // Dealer's game ending method
     public static int endingProcess(int playerScore, int dealerScore, String faceUpCard, String faceDownCard) {
+
         int newDealerScore = dealerScore;
         if (playerScore <= 21) {
-            // The Dealer's face-down card is revealed:
+
             System.out.println("\nThe Dealer's face-up card was " + faceUpCard + ".");
             System.out.println("The Dealer's face-down card is revealed: it's a " + faceDownCard + ".");
             int faceDownCardPoints = assignPoints (dealerScore, faceDownCard);
             newDealerScore = dealerScore + faceDownCardPoints;
-            System.out.println("His score is " + newDealerScore + ".\n");       
-            // The dealer plays:
+            System.out.println("His score is " + newDealerScore + ".\n");
             while (newDealerScore < 17 ) {
+
                 newDealerScore = drawNewCard(newDealerScore);
                 if (newDealerScore > 21) {
+
                     System.out.println("The dealer loses.");
                 } else if ( newDealerScore == 21 ) {
+
                     System.out.println("The Dealer's score reaches 21.");
                 } else {
+
                     System.out.println("The new Dealer's score is " + newDealerScore + ".");
                 }
             }
@@ -143,18 +157,22 @@ class Play {
         return newDealerScore;
     }
 
-    // Winner selection method
     public static void winnerSelection(int score1, int score2) {
+
         if ((score1 > 21 || score1 < score2) && (score2 < 22)) {
+
             System.out.println("The Dealer wins.\n");
         } else if ((score2 > 21 || score1 > score2) && (score1 < 22)) {
+
             System.out.println("The Player wins.\n");
         } else if ((score1 == score2) && (score1 < 22) && (score2 < 22)) {
+
             System.out.println("The Player and the Dealer are ex aequo.\n");
         }
     }
     
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
+
         int playerScore = 0;
         int dealerScore = 0;
         char answer;
